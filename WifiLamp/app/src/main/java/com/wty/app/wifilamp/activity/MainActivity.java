77cd2e4b711581b61095bfbd2f1edcd7e8809a8b
tingyouwu <wutingyou@wxchina.com>
2017-06-28 11:49:40 +0800

    修改bug

--- WifiLamp/app/src/main/java/com/wty/app/wifilamp/activity/MainActivity.java
+++ WifiLamp/app/src/main/java/com/wty/app/wifilamp/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -38,6 +39,8 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -71,6 +74,19 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
     protected void onDestroy() {
         super.onDestroy();
         EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        super.onKeyDown(keyCode, event);
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                exitBy2Click();
+                break;
+            default:
+                return super.onKeyDown(keyCode, event);
+        }
+        return false;
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -305,5 +321,27 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         sendMessage = jb.toString();
         handler.removeCallbacks(sendThread);
         handler.postDelayed(sendThread, 10);
+    }
+
+    /**
+     * 双击退出函数
+     */
+    private static Boolean isExit = false;
+    private void exitBy2Click() {
+        Timer tExit;
+        if (!isExit) {
+            isExit = true;
+            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            tExit = new Timer();
+            tExit.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    isExit = false; // 取消退出
+                }
+            }, 2000); // 如果2秒钟内没有按下返回键，则启动定时器取消掉刚才执行的任务
+
+        } else {
+            finish();
+        }
     }
 }
